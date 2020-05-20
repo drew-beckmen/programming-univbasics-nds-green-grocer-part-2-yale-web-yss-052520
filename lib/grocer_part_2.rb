19ffdee700268ac9edb10ca314b7ece0a7e69394
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-20 17:09:29 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -4,6 +4,15 @@ def apply_coupons(cart, coupons)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This method **should** update cart
+  coupons.each do |coupon|
+    find_item_by_name_in_collection(coupon[:item], cart)[:count] -= coupon[:num]
+    cart << {
+      item: coupon[:item].upcase + " W/COUPON"
+      price: coupon[:cost] / coupon[:num]
+      clearance: true 
+      count: coupon[:num]
+    }
+  end 
 end
 
 def apply_clearance(cart)
