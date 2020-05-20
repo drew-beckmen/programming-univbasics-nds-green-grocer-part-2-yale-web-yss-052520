e61a0ef57da464a01c039f1109a88cb6635667cd
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-20 17:36:08 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -29,7 +29,7 @@ end
 def apply_clearance(cart)
   cart.each do |item|
     if item[:clearance]
-      item[:price] = (item[:price] * .80).round(3)
+      item[:price] = (item[:price] * 0.8).round(3)
     end
   end
   cart
