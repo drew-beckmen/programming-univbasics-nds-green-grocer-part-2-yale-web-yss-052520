2fb4673f3ea8f176d9c2079be753a90c2b15980a
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-20 17:13:25 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -6,7 +6,9 @@ def apply_coupons(cart, coupons)
   #
   # REMEMBER: This method **should** update cart
   coupons.each do |coupon|
-    find_item_by_name_in_collection(coupon[:item], cart)[:count] -= coupon[:num]
+    find_item_by_name_in_collection(coupon[:item], cart)[:count] == coupon[:num] 
+      ?  find_item_by_name_in_collection(coupon[:item], cart)[:count] = 0 : 
+        find_item_by_name_in_collection(coupon[:item], cart)[:count] -= coupon[:num]
     binding.pry
     cart << {
       item: coupon[:item].upcase + " W/COUPON",
